ff7cb9803ed1a426516bb2689b68d940ee12370e
netss <netss@example.com>
2026-05-10 09:35:13 -0600

    feat: add black and bgWhite color codes to formatter module

--- modules/formatter.js
+++ modules/formatter.js
@@ -42,7 +42,8 @@ export const ANSI = {
     reset: "\x1b[0m", bold: "\x1b[1m", dim: "\x1b[2m",
     green: "\x1b[32m", red: "\x1b[31m", yellow: "\x1b[33m",
     blue: "\x1b[34m", cyan: "\x1b[36m", magenta: "\x1b[35m",
-    white: "\x1b[37m", gray: "\x1b[90m",
+    white: "\x1b[37m", gray: "\x1b[90m", black: "\x1b[30m",
+    bgWhite: "\x1b[47m",
 };
 
 // ---------------------------------------------------------------------------
